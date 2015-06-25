d7a0ceb31bf459af3a24a157e20cf5c2e38cd381
szewczykmira <szewczyk.mira@gmail.com>
2015-06-25 20:08:14 +0200

    Some fixes for modern ruby and strings added

--- koans/about_strings.rb
+++ koans/about_strings.rb
@@ -145,7 +145,7 @@ EOS
 
   def test_you_can_get_a_single_character_from_a_string
     string = "Bacon, lettuce and tomato"
-    assert_equal 97, string[1]
+    assert_equal "a", string[1]
 
     # Surprised?
   end
@@ -161,8 +161,8 @@ EOS
 
   in_ruby_version("1.9", "2") do
     def test_in_modern_ruby_single_characters_are_represented_by_strings
-      assert_equal __, ?a
-      assert_equal __, ?a == 97
+      assert_equal "a", ?a
+      assert_equal false, ?a == 97
     end
   end
 
